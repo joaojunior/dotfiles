f923fff7d74ab31e6a31ea0c816726b6a5504113
João Júnior <joao.junior@shopify.com>
2025-02-13 15:48:48 -0500

    Adding the .vale with the initial configuration More info: https://lwn.net/Articles/964075/ and https://www.vimfromscratch.com/articles/spell-and-grammar-vim

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 ln -sf $(realpath $(dirname $(which $BASH_SOURCE)))/.vimrc ~/.vimrc
+ln -sf $(realpath $(dirname $(which $BASH_SOURCE)))/.vale.ini ~/.vale.ini
+
+# install all packages for vale
+brew install languagetool
+vale sync
