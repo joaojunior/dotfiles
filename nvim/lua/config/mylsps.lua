f6b274306389177c1eed59aa78aa8e666f188116
João Júnior <joao.junior@shopify.com>
2025-04-10 16:05:27 -0400

    Adding conform to format code

--- nvim/lua/config/mylsps.lua
+++ nvim/lua/config/mylsps.lua
@@ -1,19 +1,18 @@
-vim.lsp.config['rubylsp'] = {
-  cmd = { "ruby-lsp" },
-  filetypes = { "ruby" },
+vim.lsp.config["rubylsp"] = {
+	cmd = { "ruby-lsp" },
+	filetypes = { "ruby" },
 }
 
-vim.lsp.config['ruby_rubocop'] = {
-  cmd = { "bundle", "exec", "rubocop", "--lsp" },
-  filetypes = { "ruby" },
+vim.lsp.config["ruby_rubocop"] = {
+	cmd = { "bundle", "exec", "rubocop", "--lsp" },
+	filetypes = { "ruby" },
 }
 
-vim.lsp.config['ruby_srb'] = {
-  cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
-  filetypes = { "ruby" },
+vim.lsp.config["ruby_srb"] = {
+	cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
+	filetypes = { "ruby" },
 }
 
-
-vim.lsp.enable('rubylsp')
-vim.lsp.enable('ruby_rubocop')
-vim.lsp.enable('ruby_srb')
+vim.lsp.enable("rubylsp")
+vim.lsp.enable("ruby_rubocop")
+vim.lsp.enable("ruby_srb")
